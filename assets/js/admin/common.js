504e4ab1f50e62d1931d07ec088fa97047641a89
hiennvth1907052 <hiennvth1907052@fpt.edu.vn>
2021-10-30 01:34:09 +0700

    1

--- assets/js/admin/common.js
+++ assets/js/admin/common.js
@@ -1,12 +1,42 @@
-$(document).ready(function() {
+// $(document).ready(function() {
+    "use strict"
     // get token
-    var token;
+    var token, pageContent, currentUserName;
     function startLoad(cookie){
-        token = cookie || '';
+        console.log(cookie)
+        if (cookie) {
+            token = getCookie('token', cookie);
+            currentUserName = getCookie('username', cookie);
+        }
+        if (token) {
+            creaElement('div', 'container', '', 'body', 'afterbegin');
+            creaElement('div', 'wrapper', 'root', '.container', 'afterbegin');
+            insertHtml('#root', '<a href="javaScript:;" class="back-to-top"><i class="bx bxs-up-arrow-alt"></i></a>', 'afterbegin');
+            insertHtml('#root', '<div class="overlay toggle-icon"></div>', 'afterbegin');
+            loadHtml('head.html', '#root', 'header', '', '', 'afterbegin', '../../../assets/js/admin/header.js');
+            loadHtml('sidebar.html', '#root', 'div', 'sidebar-wrapper', '', 'afterbegin', '../../../assets/js/admin/sidebar.js');
+            loadHtml('setBg.html', '.container', 'div', 'switcher-wrapper', '', 'afterend', '../../../assets/js/admin/setBg.js');
+        } else {
+            loadHtml('login.html', 'body', 'div', 'wrapper', '', 'afterbegin', '../../../assets/js/admin/login.js');
+        }
     }
     startLoad(document.cookie);
-    function getToken (){
-        
+    function getCookie (key, cookie){
+        if (cookie) {
+            var value;
+            var split = cookie.split(';');
+            if (split && split.length > 0) {
+                for (let index = 0, len = split.length; index < len; index++) {
+                    var element = split[index].trim();
+                    if (element.indexOf(key) != -1) {
+                        value = element.split('=')[1];
+                    }
+                }
+            }
+            return value;
+        } else {
+            return '';
+        }
     }
     function creaElement(ele, className, id, position, position1){
         var positionElement = document.querySelectorAll(position);
@@ -53,17 +83,24 @@ $(document).ready(function() {
         .catch(error => console.log(console.log(error)));
         
     }
-    
-    if (token) {
-        creaElement('div', 'container', '', 'body', 'afterbegin');
-        creaElement('div', 'wrapper', 'root', '.container', 'afterbegin');
-        insertHtml('#root', '<a href="javaScript:;" class="back-to-top"><i class="bx bxs-up-arrow-alt"></i></a>', 'afterbegin');
-        insertHtml('#root', '<div class="overlay toggle-icon"></div>', 'afterbegin');
-        loadHtml('head.html', '#root', 'header', '', '', 'afterbegin', '../../../assets/js/admin/header.js');
-        loadHtml('sidebar.html', '#root', 'div', 'sidebar-wrapper', '', 'afterbegin', '../../../assets/js/admin/sidebar.js');
-        loadHtml('setBg.html', '.container', 'div', 'switcher-wrapper', '', 'afterend', '../../../assets/js/admin/setBg.js');
-    } else {
-        loadHtml('login.html', 'body', 'div', 'wrapper', '', 'afterbegin', '../../../assets/js/admin/login.js');
+
+    function getConnectAPI(method, url, dataPost, successCallback, failCallback){
+        fetch(url, {
+            method: method,
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: dataPost ? JSON.stringify(dataPost) : null,
+        })
+        .then((response) => response.json())
+        .then(function (data) {
+            if (successCallback) {
+                successCallback(data);
+            }
+        })
+        .catch(function (error) {
+            failCallback();
+        });
     }
-    
-});
+// });
