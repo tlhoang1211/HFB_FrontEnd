bf203a11fc1b9c5b1595c31661811f4d3b5362d1
Hoàng Trần <46571690+tlhoang1211@users.noreply.github.com>
2021-11-01 02:30:34 +0700

    1

--- assets/js/account_pagination.js
+++ assets/js/account_pagination.js
@@ -1,1 +1,104 @@
+// hoangtl2 - 01/11/2021 - food list pagination on account page
+// start
 
+var dataAccount = null;
+var foodListAPI =
+  "https://hfb-t1098e.herokuapp.com/api/v1/hfb/foods/search?status=2";
+var shopItem = document.querySelector(".list-food-item");
+function getListFood() {
+  fetch(foodListAPI, {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((listItems) => {
+      renderListFood(listItems.data.content);
+    })
+    .catch((error) => console.log(error));
+}
+getListFood();
+
+function renderListFood(listFood) {
+  let container = $(".pagination");
+  container.pagination({
+    dataSource: listFood,
+    pageSize: 5,
+    showGoInput: true,
+    showGoButton: true,
+    formatGoInput: "go to <%= input %>",
+    callback: function (data, pagination) {
+      var dataHtml = "<div>";
+      var count = 0;
+      $.each(data, function (index, e) {
+        count++;
+        // console.log(e.name);
+        dataHtml +=
+          `<tr>
+        <td>${count}</td>
+        <td>${e.name || ""}</td>
+        <td><img src="https://res.cloudinary.com/vernom/image/upload/${
+          e.avatar
+        }" style="width: 30px;height: 30px;"/></td>
+        <td>${formatCategory(e.categoryId)}</td>
+        <td>${e.expirationDate}</td>
+        <td>${
+          e.status == 0 ? "deactive" : e.status == 1 ? "pending" : "active"
+        }</td>
+        <td>${e.createdAt}</td>
+        <td onclick="formUpdateFood()"><i class="fa fa-pencil-square-o"></i></td>` +
+          `<td onclick=confirmDeleteFood()><i class="fa fa-trash-o"></i></td></tr>`;
+      });
+
+      dataHtml += "</div>";
+
+      $("#list-food").html(dataHtml);
+    },
+  });
+}
+//end
+
+// hoangtl2 - 01/11/2021 - food list pagination on account page
+// start
+// update food
+function formUpdateFood() {}
+
+// display donate modal on click delete button
+var modal3 = document.querySelector(".modal-account-confirm-delete");
+function confirmDeleteFood() {
+  modal3.style.display = "flex";
+}
+
+// delete food
+function deleteFood(id) {
+  var dataPost = {
+    status: 0,
+  };
+  fetch(`https://hfb-t1098e.herokuapp.com/api/v1/hfb/foods/status/${id}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${isToken}`,
+    },
+    body: JSON.stringify(dataPost),
+  })
+    .then((response) => response.json())
+    .then((food) => {
+      if (food) {
+        e.parentElement.remove();
+      }
+    })
+    .catch((error) => console.log(error));
+}
+
+// Close Modal by clicking "close" button
+function closeModal() {
+  modal3.style.display = "none";
+}
+
+// Close Modal by clicking "esc" button
+$(document).keydown(function (event) {
+  if (event.keyCode == 27) {
+    modal3.style.display = "none";
+    event.preventDefault();
+  }
+});
+// end
--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -1,18 +1,17 @@
-function paginationFood(listFood){
-    let container = $('.pagination');
-    container.pagination({
-        dataSource: listFood,
-        pageSize: 8,
-        showGoInput: true,
-        showGoButton: true,
-        formatGoInput: 'go to <%= input %>',
-        callback: function (data, pagination) {
-            let paginationFoodPromise = new Promise(function(myResolve) {
-                var dataHtml = '<div>';
+function paginationFood(listFood) {
+  let container = $(".pagination");
+  container.pagination({
+    dataSource: listFood,
+    pageSize: 8,
+    showGoInput: true,
+    showGoButton: true,
+    formatGoInput: "go to <%= input %>",
+    callback: function (data, pagination) {
+      let paginationFoodPromise = new Promise(function (myResolve) {
+        var dataHtml = "<div>";
 
-                $.each(data, function (index, item) {
-                    dataHtml +=
-                    `<div class="col-sm-6 col-md-3 col-lg-3" style="min-height: 350px;" id="pg-shop-item-${item.id}">
+        $.each(data, function (index, item) {
+          dataHtml += `<div class="col-sm-6 col-md-3 col-lg-3" style="min-height: 350px;" id="pg-shop-item-${item.id}">
                     <div class="shop-item">
                         <div class="shop-item-image"><img class="img-food" src="https://res.cloudinary.com/vernom/image/upload/${item.avatar}" class="img-food" alt="Cold Garb"/>
                         <div class="shop-item-detail"><a class="btn btn-round btn-b" href="./shop_single_product.html?id=${item.id}"><span class="icon-basket">View Details</span></a></div>
@@ -20,42 +19,42 @@ function paginationFood(listFood){
                         <h4 class="shop-item-title font-alt"><a href="#">${item.name}</a></h4>
                         <p>Expiration Date: ${item.expirationDate}</p>
                     </div>
-                    </div>`
-                });
+                    </div>`;
+        });
 
-                dataHtml += '</div>';
+        dataHtml += "</div>";
 
-                $("#data-container").html(dataHtml);
-                myResolve();
-            })
+        $("#data-container").html(dataHtml);
+        myResolve();
+      });
 
-            paginationFoodPromise.then(
-                function() { 
-                  data.map(function(item2){
-                    var tet = getTimeFromString3(item2.expirationDate);
-                    run();
-                    
-                    // Tổng số giây 
-                    var countDown = setInterval(run,1000);
-                    function run(){
-                        var now = new Date().getTime();
-                        var timeRest = tet - now;
-                        if(timeRest <= 0){
-                          clearInterval(countDown);
-                          document.querySelector(`#pg-shop-item-${item2.id}`).style.display = 'none';
-                        }
-                        }
-                  })
-                }
-            );
-        }
-    })
-  }
+      paginationFoodPromise.then(function () {
+        data.map(function (item2) {
+          var tet = getTimeFromString3(item2.expirationDate);
+          run();
+
+          // Tổng số giây
+          var countDown = setInterval(run, 1000);
+          function run() {
+            var now = new Date().getTime();
+            var timeRest = tet - now;
+            if (timeRest <= 0) {
+              clearInterval(countDown);
+              document.querySelector(
+                `#pg-shop-item-${item2.id}`
+              ).style.display = "none";
+            }
+          }
+        });
+      });
+    },
+  });
+}
 
 function getTimeFromString3(strDate) {
-    var arrDate = strDate.split('/');
-    var year = parseInt(arrDate[2]);
-    var month = parseInt(arrDate[1]) - 1;
-    var date = parseInt(arrDate[0]);
-    return new Date(year, month, date).getTime();
+  var arrDate = strDate.split("/");
+  var year = parseInt(arrDate[2]);
+  var month = parseInt(arrDate[1]) - 1;
+  var date = parseInt(arrDate[0]);
+  return new Date(year, month, date).getTime();
 }
